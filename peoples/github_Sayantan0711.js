ce289039a6c065f89313bcf4e97ea8b745485ffd
Sayantan0711 <72100041+Sayantan0711@users.noreply.github.com>
2020-10-01 12:57:05 +0530

    Create github_Sayantan0711.js

---
+++ peoples/github_Sayantan0711.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Sayantan MItra',
+    github: 'Sayantan0711',
+    email: '1807217@kiit.ac.in',
+    twitter: '@xxx',
+    facebook: 'xyx',
+    linkedin: 'in/xxx'
+  };
